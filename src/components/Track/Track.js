e8991bfeaf78aa39a026a67fdec175e9875d8070
sellayuniar <synrr212@gmail.com>
2022-04-02 20:23:47 +0800

    fitur deselect sudah bisa

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -1,6 +1,5 @@
 
-
-const Track = ({item, onSelectedTrack}) =>{ 
+const Track = ({item, onSelectTrack}) =>{ 
     const {album, artists, name: songName, isSelected, uri} = item;
     
     return (
@@ -9,7 +8,7 @@ const Track = ({item, onSelectedTrack}) =>{
         <img src={album.images[0].url} alt=""/>
         <h3>{songName}</h3>
         <h2>{artists.map(artist => artist.name).join(',')}</h2>
-        <button onClick={()=>{onSelectedTrack(uri)}}> {isSelected? 'Deselect' : 'Selected'}</button>
+        <button onClick={()=>{onSelectTrack(uri)}}> {isSelected? 'Deselect' : 'Selected'}</button>
         </div>
         </div>
 )
