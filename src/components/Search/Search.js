f6a4534942d07fc44aa1d0cf1376d3ae5e30ed2b
sellayuniar <synrr212@gmail.com>
2022-04-13 21:59:27 +0800

    added MUI style to search bar

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,14 +1,14 @@
-import styles from "./search.module.css";
-
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 const Search = ({accessToken, handleOnChange, data,  getTracks, TrackLoop}) => {
     
     return (
         <div>
-            <div  className={styles.searchSection}> 
+            <div> 
             <h1>Search Song</h1>
-            <input className="search" type="text" onChange={handleOnChange} placeholder="Type to Search"/>
-            <button onClick={()=>{getTracks(accessToken)}} > Search</button>
+            <TextField id="outlined-basic" label="Search your favorite song!" variant="outlined" onChange={handleOnChange} />
+            <Button  variant="contained" color="success" size="large" onClick={()=>{getTracks(accessToken)}} > Search</Button>
             </div>
             
             {data !== undefined && (
